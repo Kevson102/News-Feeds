e37429044103eac7d7956a8c196d6cf90061f8fd
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-10-29 17:07:16 +0300

    CREATES THE CONFIG, DEVCONFIG AND THE PRODCONFIG CLASSES IN THE CONFIG.PY FILE

---
+++ app/config.py
@@ -0,0 +1,20 @@
+class Config():
+  '''
+  This is the general configuration class
+  '''
+  pass
+
+class ProdConfig(Config):
+  '''
+  This is the configuration configuration class
+  Args:
+    Config: The parent configuration class that holds the general configurations
+  '''
+  pass
+
+class  DevConfig(Config):
+  '''
+  This is the development configuration class.
+  Args:
+    Config: The parent configuration class that holds the general configurations
+  '''
