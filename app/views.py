1a0282e3fdbbd0ec75231edcd7b14cb9a6e3ccb7
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-10-29 17:31:05 +0300

    SAVES THE NEWS_API_KEY IN THE INSTANCE/CONFIG.PY FILE

--- app/views.py
+++ app/views.py
@@ -7,12 +7,13 @@ def index():
   '''
   View root page function that returns the index page and its data.
   '''
-  message = 'INDEX PAGE WORKS'
-  return render_template('index.html', message=message)
+  title = 'Welcome to the most credible news platform | NewsPap'
+  return render_template('index.html', title=title)
 
 @app.route('/news/<int:news_id>')
 def news(news_id):
   '''
   View function that returns the news details page and its respective data.
   '''
-  return render_template('news.html', id = news_id)
+  title = 'Headline Description'
+  return render_template('news.html', id = news_id, title=title)
